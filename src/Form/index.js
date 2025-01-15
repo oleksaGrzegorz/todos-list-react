7ba0d77b102c0e2b3d6af88ec90a67fe569fbc8d
Grzegorz Oleksa <grzesieq92@gmail.com>
2025-01-15 15:11:23 +0100

    input is controlled component

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,8 +1,19 @@
-const Form = () => (
-  <form className="section__form">
-    <input className="section__input" placeholder="Co jest do zrobienia?" />
-    <button className="section__button">Dodaj zadanie</button>
-  </form>
-);
+import { useState } from "react";
+
+const Form = () => {
+  const [newTaskContent, setNewTaskContent] = useState("");
+
+  return (
+    <form className="section__form">
+      <input
+        value={newTaskContent}
+        onChange={({ target }) => setNewTaskContent(target.value)}
+        className="section__input"
+        placeholder="Co jest do zrobienia?"
+      />
+      <button className="section__button">Dodaj zadanie</button>
+    </form>
+  );
+};
 
 export default Form;
