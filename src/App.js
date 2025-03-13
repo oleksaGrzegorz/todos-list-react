2086ebb238f1fb9acd2a593439b8b94109b57829
Grzegorz Oleksa <grzesieq92@gmail.com>
2025-03-13 11:45:00 +0100

    router - added routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-import Tasks from "./features/tasks/Tasks";
+import { Routes, Route, HashRouter, Navigate } from "react-router-dom";
+import TasksPage from "./features/tasks/TasksPage";
+import AuthorPage from "./features/author/AuthorPage";
+import TaskPage from "./features/tasks/TaskPage";
+import StyledNavLink from "./styled";
 
-const App = () => <Tasks />;
+const App = () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <StyledNavLink to="/zadania">Zadania</StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/autor">O autorze</StyledNavLink>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/zadania/:id" element={<TaskPage />} />
+      <Route path="/zadania" element={<TasksPage />} />
+      <Route path="/autor" element={<AuthorPage />} />
+      <Route path="/" element={<Navigate to="/zadania" />} />
+    </Routes>
+  </HashRouter>
+);
 
 export default App;
