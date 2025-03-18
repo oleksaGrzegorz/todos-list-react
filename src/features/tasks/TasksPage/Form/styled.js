d4921defde1cdb9aa5f8d6b05647dae57b13de9e
Grzegorz Oleksa <grzesieq92@gmail.com>
2025-03-18 12:15:56 +0100

    disabled button styled

--- src/features/tasks/TasksPage/Form/styled.js
+++ src/features/tasks/TasksPage/Form/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+
 export const Button = styled.button`
   color: white;
   background-color: hsl(180, 100%, 25%);
@@ -19,5 +20,29 @@ export const StyledForm = styled.form`
 
   @media (max-width: 757px) {
     flex-direction: column;
+  }
+`;
+
+export const FetchExampleTasksContainer = styled.div`
+  text-align: right;
+  padding: 10px;
+`;
+
+export const FetchExampleTasksButton = styled.button`
+  color: hsl(180, 100%, 25%);
+  background-color: white;
+  text-decoration: none;
+  cursor: pointer;
+  border: none;
+  text-align: left;
+  transition: background-color 0.3s ease, color 0.3s ease;
+
+  &:hover {
+    color: hsl(180, 100%, 35%);
+  }
+
+  &:disabled {
+    color: gray;
+    cursor: not-allowed;
   }
 `;
